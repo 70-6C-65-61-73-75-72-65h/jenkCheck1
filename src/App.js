6e021799fe2697b2ee1fb4716cbbe88a46be8571
70-6C-65-61-73-75-72-65h <max.ulshin.max@gmail.com>
2023-06-15 03:43:44 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reac,
+          Я энгри берд и я ебал ваш рот
         </a>
       </header>
     </div>
